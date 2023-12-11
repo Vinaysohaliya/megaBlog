f87cf97c7d1a7c6e827827abd8240b93132dec7c
Vinaysohaliya <sohaliyavin01@gmail.com>
2023-12-11 10:07:29 +0530

    header footer and some form funtion added

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "../container/Container";
+import Logo from "./Logo";
+import Logout from "./Header/Logout";
+
 
 export {
-    Header, Footer
+    Container,
+    Header,
+    Footer,
+    Logout
 }
