ab701a84264d782aa181f41498ebed75bea2f895
Vinaysohaliya <sohaliyavin01@gmail.com>
2023-12-11 16:32:35 +0530

    auth form builded

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,15 @@ import Footer from "./Footer/Footer";
 import Container from "../container/Container";
 import Logo from "./Logo";
 import Logout from "./Header/Logout";
+import Input from "./input";
+
 
 
 export {
     Container,
     Header,
     Footer,
-    Logout
+    Logout,
+    Logo,
+    Input
 }
